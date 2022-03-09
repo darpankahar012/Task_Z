b066c2fb7cac97327f5341871caad40e9b8fddd8
darpan@webmynesystems.com <darpan@webmynesystems.com>
2022-03-09 17:42:09 +0530

    first commit

---
+++ public/asset/js/custom.js
@@ -0,0 +1,28 @@
+/* For Sticky Header */
+jQuery(window).scroll(function () {
+    var sc = jQuery(window).scrollTop()
+    if (sc > 50) {
+        jQuery("#myHeader").addClass("small")
+    } else {
+        jQuery("#myHeader").removeClass("small")
+    }
+});
+/* End */
+
+/* For OTP Screen */
+let digitValidate = function(ele){
+    console.log(ele.value);
+    ele.value = ele.value.replace(/[^0-9]/g,'');
+  }
+  
+  let tabChange = function(val){
+      let ele = document.querySelectorAll('input');
+      if(ele[val-1].value != ''){
+        ele[val].focus()
+      }else if(ele[val-1].value == ''){
+        ele[val-2].focus()
+      }   
+   }
+  
+  
+/* End */
